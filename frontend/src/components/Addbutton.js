041564fde0f731a09e0de64724e0863b92d2bbe0
Yusf4 <youssefmazloum2017@gmail.com>
2024-09-04 15:56:49 -0400

    test

--- frontend/src/components/Addbutton.js
+++ frontend/src/components/Addbutton.js
@@ -5,7 +5,7 @@ const Addbutton=({product,addToBag})=>{
         <div>
         <button
         className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none"
-        onClick={()=>addToBag(product)}
+        onClick={()=>addToBag(pproduct)}
         >
             Add to Bag
         </button>
