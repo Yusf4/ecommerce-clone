6c60b7078a660fb2055314b5658b748da990c16c
Yusf4 <youssefmazloum2017@gmail.com>
2024-09-13 18:10:42 -0400

    clean the code

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -47,7 +47,8 @@ export default LoginPage;*/
 import api from '../axiosConfig';
 import { useState } from 'react';
 import axios from 'axios';
-
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 const LoginPage = () => {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
 
@@ -69,16 +70,55 @@ const LoginPage = () => {
     }
   };
 
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        <input type="email" name="email" onChange={handleChange} placeholder="email" />
-      </label>
-      <label htmlFor="password">
-        <input type="password" name="password" onChange={handleChange} placeholder="password" />
-      </label>
-      <button type="submit">Login</button>
-    </form>
+    <div className="min-h-screen flex flex-col justify-between">
+   
+    <Header />
+
+    <div className="flex flex-col items-center justify-center flex-grow">
+      <form 
+        onSubmit={handleSubmit} 
+        className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-md space-y-6"
+      >
+   
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-gray-700 mb-2">Email</label>
+          <input 
+            type="email" 
+            name="email" 
+            onChange={handleChange} 
+            placeholder="email" 
+            className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            required
+          />
+        </div>
+
+  
+        <div className="flex flex-col">
+          <label htmlFor="password" className="text-gray-700 mb-2">Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            onChange={handleChange} 
+            placeholder="password" 
+            className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            required
+          />
+        </div>
+
+        <button 
+          type="submit" 
+          className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+
+ 
+    <Footer />
+  </div>
   );
 };
 
