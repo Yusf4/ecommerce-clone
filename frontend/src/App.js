b49e01be13064567249e0473814ed5e3ee7be588
Yusf4 <youssefmazloum2017@gmail.com>
2024-09-15 16:55:48 -0400

    review code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
 import Register from './pages/RegisterPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import CategoriesPage from './pages/CategoriesPage';
