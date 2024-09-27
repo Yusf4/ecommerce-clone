5a0fe17167827f058bd3f959a0da391eb3dcc04c
Yusf4 <youssefmazloum2017@gmail.com>
2024-09-27 18:17:42 -0400

    implementing order logic in the backend

--- backend/app/Http/Controllers/OrderController.php
+++ backend/app/Http/Controllers/OrderController.php
@@ -1,11 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Order;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+
+    public function createOrder(Request $request){
+        $order=Order::create([
+            'user_id'=>Auth::id(),
+            'total'=>$request->total,
+            'status'=>'pending',
+        ]);
+        foreach($request->bag as $item){
+            $order->products()->create([
+                'product_id'=>$item['product']['id'],
+                'quantity'=>$item['quantity'],
+            ]);
+        }
+        return response()->json(['order_id'=>$order->id]);
+    }
     /**
      * Display a listing of the resource.
      */
